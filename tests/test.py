6816480ae53d0c2cb4c57c5eb78016dc03abfa25
JeongUkJae <JeongUkJae@gmail.com>
2017-08-29 22:21:14 +0900

    edit testcase name

--- tests/test.py
+++ tests/test.py
@@ -1,7 +1,7 @@
 import unittest
 from app import create_app
 
-class FlaskrTestCase(unittest.TestCase):
+class CIExampleTestCase(unittest.TestCase):
 
     def setUp(self):
         self.app = create_app()
